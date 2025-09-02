34996f311c3b542cd861a28dc89b0b3a2b8a61b2
Mario Brino <mariobrino@gmail.com>
2025-09-01 21:49:21 -0600

    chore: added assignment exercise 5 - functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,42 @@ using System;
 
 class Program
 {
+    public static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    public static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string fullname = Console.ReadLine();
+        return fullname;
+    }
+
+    public static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favoritNumber = int.Parse(Console.ReadLine());
+        return favoritNumber;
+    }
+
+    public static int SquareNumber(int number)
+    {
+        int squareNumber = number * number;
+        return squareNumber;
+    }
+
+    public static void DisplayResult(string fullname, int squareNumber)
+    {
+        Console.WriteLine($"{fullname}, the square of your number is {squareNumber}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string fullname = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squareNumber = SquareNumber(userNumber);
+        DisplayResult(fullname, squareNumber);
     }
 }
